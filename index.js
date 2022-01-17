1353843052de966a3654c7f3b8d4daf525cce9cb
root <root@DESKTOP-UHS28DU.localdomain>
2022-01-16 21:20:09 -0500

    commit tree 6479b777f336ee0dfbfc0bfc8ef766398b9afae1

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+ function logShout(spy){
+    console.log('HELLO');
+ }
+ 
+ function logWhisper(spy){
+     console.log('hello')
+ }
+
+function sayHiToHeadphonedRoommate(string){
+    if ( spy === toLowerCase('hello') {
+        console.log(string)
+    return string.toLowerCase('I can/t hear you!') ;
+    }
+}
+
+function sayHiToHeadphonedRoommate(spy){
+    if ( spy === toUpperCase('HELLO')) {
+        console.log(string);
+    return "YES INDEED!";
+    }
+}
+
+function sayHiToHeadphonedRoommate(spy){
+    if ( spy  === "Let\'s have dinner together!") {
+        console.log(string);
+    return  "I would love to!";
+    }
+}
+
+
+
+
+
+
